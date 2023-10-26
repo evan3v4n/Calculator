bf08db636437c1e0a933fda97e682ad0d65412eb
evan3v4n <evan.scott2050@Gmail.com>
2023-10-26 18:06:00 -0400

    User hits a number key

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 const calculator = document.querySelector('.calculator');
 const keys = document.querySelector('.calculator__keys');
 
+const display = document.querySelector('.calculator__display');
+
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
+
         const key = e.target;
         const action = key.dataset.action;
 
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+
         if (!action) {
             console.log("Number key!")
-        }
+            if (displayedNum === "0"){
+                display.textContent = keyContent
+                console.log(displayedNum)
+            } else {
+                display.textContent = displayedNum + keyContent
+                console.log(displayedNum)
+            }
+        };
         
         if (
-            action == 'add' ||
-            action == 'subtract' ||
-            action == 'multiply' ||
-            action == 'divide' 
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide' 
             ) {
             console.log("Operator key!")
         }
 
-        if (action == 'decimal') {
+        if (action === 'decimal') {
             console.log("Decimal key!")
         }
 
-        if (action == 'clear') {
+        if (action === 'clear') {
             console.log("Clear key!")
         }
 
-        if (action == 'calculate') {
+        if (action === 'calculate') {
             console.log("Equals key!")
         }
     }
